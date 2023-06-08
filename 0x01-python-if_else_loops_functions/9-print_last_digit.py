496055827cf8dbdc6b0c7d6271b4c37ce531d89a
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-08 05:42:03 +0000

    if else

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = 0
+    if number >= 0:
+        last_digit = number % 10
+    else:
+        last_digit = 10 - (number % 10)
+
+    print('{:d}'.format(last_digit), end='')
+    return last_digit
