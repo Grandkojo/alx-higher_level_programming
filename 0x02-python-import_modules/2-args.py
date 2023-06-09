0314262a9c7b84349c555c23654f52be19391c22
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-09 01:37:15 +0000

    Importing Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import sys
 
-arguments = sys.argv[1:]
+if __name__ == '__main__':
+	import sys
 
-length = 0
-for arg in arguments:
-    length += 1
-print(f"{length} arguments:")
+	arguments = sys.argv[1:]
 
-for i in range(0, length):
-    print(f"{i+1}: {arguments[i]}")
+	length = 0
+	for arg in arguments:
+    		length += 1
+	print(f"{length} arguments:")
+
+	for i in range(0, length):
+    		print(f"{i+1}: {arguments[i]}")
           
 
