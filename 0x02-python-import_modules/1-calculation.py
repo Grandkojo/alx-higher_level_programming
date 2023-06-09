070dec13450e3b6d0cbe48eeaebbb81ec8c9ea9e
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-09 00:49:17 +0000

    Importing modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,11 @@
 import calculator_1
 
 if __name__ == '__main__':
-	calculator_1.add(10, 5)
-	calculator_1.sub(10, 5)
-	calculator_1.mul(10, 5)
-	calculator_1.div(10, 5)
+
+	a = 10
+	b - 5
+
+	calculator_1.add(a, b)
+	calculator_1.sub(a, b)
+	calculator_1.mul(a, b)
+	calculator_1.div(a, b)
---
+++ 0x02-python-import_modules/calculator-1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    a = 10
+    b = 5
+    result = a + b
+    print(f"{a} + {b} = {result}")
+
+def sub(a, b):
+    a = 10
+    b = 5
+    result = a - b
+    print(f"{a} - {b} = {result}")
+
+def mul(a, b):
+    a = 10
+    b = 5
+    result = a * b
+    print(f"{a} * {b} = {result}")
+
+def div(a, b):
+    a = 10
+    b = 5
+    result = int(a / b)
+    print(f"{a} / {b} = {result}")
