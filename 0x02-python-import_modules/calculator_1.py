58f9e0219aa954aa54eac77ae5cbedbc10aa3bbc
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-09 00:59:01 +0000

    Importing Modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    a = 10
+    b = 5
+    return( a + b)
+
+def sub(a, b):
+    a = 10
+    b = 5
+    return(a - b)
+
+def mul(a, b):
+    a = 10
+    b = 5
+    return (a * b)
+
+def div(a, b):
+    a = 10
+    b = 5
+    return int(a / b)
