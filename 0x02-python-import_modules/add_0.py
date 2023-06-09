36f78919d4c833910ca4133c3063b1091bcb4d9e
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-09 00:07:59 +0000

    import modules

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-	def add(a, b):
-		a = 1
-		b = 2
-		result = a + b
-	        print(f"{a} + {b} = {result}")
+def add(a, b):
+	a = 1
+	b = 2
+	result = a + b
+	print(f"{a} + {b} = {result}")
 
