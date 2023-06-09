a951be33d2b12f06454b190333af736b641b87b4
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-09 01:48:30 +0000

    Importing Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,12 @@ if __name__ == '__main__':
 	length = 0
 	for arg in arguments:
     		length += 1
-	print(f"{length} arguments:")
+	if length == 0:
+		print("0 arguments")
+	elif length == 1:
+		print("1 argument")
+	else:	
+		print(f"{length} arguments:")
 
 	for i in range(0, length):
     		print(f"{i+1}: {arguments[i]}")
-          
-
