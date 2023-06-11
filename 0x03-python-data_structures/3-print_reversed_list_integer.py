7f42c2c47e4c5d225b0e85871bfd20dd8881eab2
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-11 23:06:12 +0000

    Data Structuresc

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
 	if my_list:
 		my_list.reverse()
 		for k in range(len(my_list)):
-			print("{:d}".format(my_list[i]))
+			print("{:d}".format(my_list[k]))
