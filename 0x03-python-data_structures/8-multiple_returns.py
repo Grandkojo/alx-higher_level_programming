5a15b24a284d24365f8aab6e77ba2739b33ed5f6
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-12 09:02:05 +0000

    Data Structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def multiple_returns(sentence)
+def multiple_returns(sentence):
 	new_tuple = ()
-	if len(sentence) == 0
+	if len(sentence) == 0:
 		new_tuple = 0, None
 	else:
 		new_tuple = len(sentence), sentence[0]
