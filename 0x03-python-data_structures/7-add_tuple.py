d1ff7436cb76f93e1bdc23b7138d4e97febfb000
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-12 08:40:28 +0000

    Data Structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
 	my_tuple = ()
 	tuple_x = tuple_a + (0, 0)
-	tuple_y = tuple_b = (0, 0)
+	tuple_y = tuple_b + (0, 0)
 	my_tuple = tuple_x[0] + tuple_y[0], tuple_x[1] + tuple_y[1]
 	return my_tuple
