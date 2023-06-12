7b854e92fbeaec7966dd6cbef5ff5d29b6aa777a
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-06-12 09:18:21 +0000

    Data Structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 	max = 0
+	if my_list is None:
+       		return
+   	if len(my_list) == 0:
+        	return
 	for i in range(len(my_list)):
 		if my_list[i] >= max:
 			max = my_list[i]
