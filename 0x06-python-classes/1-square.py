7f9cd502adace4acd8147cf2a97af7abda975db5
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-07-14 08:36:19 +0000

    Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''A sqaure class with size'''
+
+class Square:
+'''Defining a class square'''
+	def __init__(self, size):
+		'''Initiating a square class
+		Args: size: size of the square
+		'''
+		self.__size = size
