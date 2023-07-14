a16342bcc4cc6ddcbbd7dfc8a2060b9d92b80594
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-07-14 10:01:14 +0000

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""Creating a square class"""
+""" Creating a square class """
 
 
 class Square:
-	"""Defining a square class"""
-	def __init__(self, size=0):
-        	"""Initializing a square class
-        	Args: size=0: size of the square
-       		"""
-		self.__size = size
+    """ Defining a class square """
+    def __init__(self, size=0):
+        """ Initializing a square class
+        Args: size=0: size of the square
+         """
+        self.__size = size
 
-	@property
-	def size(self):
-		"""getting the size of the square"""
-		return sef.__size
-	
-	@size.setter
-	def size(self, value):
-		"""Setting the size of the square"""
-        	if type(size is not int:
-                	raise TypeError("size must be an integer")
-      		if size < 0:
-                	raise ValueError("size must be >= 0")
-        	self.__size = value
-	
-	def area(self):
-		"""Calculating area of the square"""
-		return (self.__size ** 2)
+    @property
+    def size(self):
+        """ Getting the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setting the size of the square """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Calculating the area of the square """
+        return (self.__size ** 2)
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-"""Creating a square class"""
+""" Creating a square class """
 
 
 class Square:
-	"""Defining a square class"""
-	def __init__(self, size=0):
-        	"""Initializing a square class
-        	Args: size=0: size of the square
-       		"""
-		self.__size = size
+    """ Defining a class square """
+    def __init__(self, size=0):
+        """ Initializing a square class
+        Args: size=0: size of the square
+         """
+        self.__size = size
 
-	@property
-	def size(self):
-		"""getting the size of the square"""
-		return sef.__size
-	
-	@size.setter
-	def size(self, value):
-		"""Setting the size of the square"""
-        	if type(value) is not int:
-                	raise TypeError("size must be an integer")
-      		if value < 0:
-                	raise ValueError("size must be >= 0")
-        	self.__size = value
-	
-	def area(self):
-		"""Calculating are of the squarea"""
-		return (self.__size ** 2)
+    @property
+    def size(self):
+        """ Getting the size of the square """
+        return self.__size
 
-	def my_print(self):
-		"""Printing the square"""
-		if self.__size == 0:
-			print()
-		else:
-			for item in range(self.__size):
-				print("#" * self.__size)
+    @size.setter
+    def size(self, value):
+        """ Setting the size of the square """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Calculating the area of the square """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ Printing the square """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
