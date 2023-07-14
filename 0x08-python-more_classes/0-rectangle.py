0c2b3949f7213228f0ddc73eeda5d5b76d7b386d
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-07-14 15:14:08 +0000

    More Classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Creating a rectangle class"""
+""" Creating a rectangle class """
 
 
 class Rectangle:
-	"""Create a rectangle class"""
+	""" Creating a rectangle class """
 	pass
