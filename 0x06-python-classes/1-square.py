ecc2da64af4132f1b7fb8154fa110368ba9bb8ed
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-07-14 08:43:49 +0000

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''A sqaure class with size'''
 
+
 class Square:
-'''Defining a class square'''
+	'''Defining a class square'''
 	def __init__(self, size):
 		'''Initiating a square class
 		Args: size: size of the square
