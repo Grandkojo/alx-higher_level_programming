7387f3a9ba6b29cf7e7ddd34d9b1e08bf49af38b
Grandkojo <103688380+Grandkojo@users.noreply.github.com>
2023-08-30 20:26:17 +0000

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string():
-    setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
-    return 'Holberton' + ', Holberton' * getattr(magic_string, 'rep', 0)
--- 0x09-python-everything_is_object/README.md
+++
@@ -1,1 +0,0 @@
-Python everything is object
