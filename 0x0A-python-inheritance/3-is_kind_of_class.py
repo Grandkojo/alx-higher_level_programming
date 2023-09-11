1b3a530b4a49925173a1d3385c69b5c27f96de04
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-11 21:14:53 +0000

    Python Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains only one function"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the object is an instance of the specified class"""
+    return isinstance(obj, a_class)
