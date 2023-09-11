4033dda100c67423e0ba44429eb073afd822ca20
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-11 21:11:02 +0000

    Python Inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This module contains only one function"""
+"""THis module contains only one function"""
 
 
 def is_same_class(obj, a_class):
-   """Checks if the object is exactly an instance of the specified class, otherwise"""
-   return (type(obj) is a_class))
+    """Check is obj is a class member"""
+    return (type(obj) is a_class)
