2399c546528562cd3e11efb751f97a8b5514af7c
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-11 20:04:26 +0000

    Pyton Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains a function called lookup """
+
+def lookup(obj):
+	"""The lookup function
+	Args:
+		The object to be received
+	Returns:
+		A list of all the available attributes and methods of the object
+	"""
+	attributes_and_methods = dir(obj)
+	return attributes_and_methods
