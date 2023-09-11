19895ae4c276551e199956c8d67bad847f215cfd
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-11 21:49:15 +0000

    Python Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module has only one class"""
+
+
+class BaseGeometry():
+    """This class is a geometry class"""
+
+    def area(self):
+        """finds the area of the class"""
+        raise Exception("area() is not implemented")
