f95c33c1dd93451a82153dc769523e854c6a664d
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-11 20:47:07 +0000

    Python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""This modules contains two classes and a function"""
+"""THis module checks the list"""
 
 
-class MyList(List):
-   """This is an inherited class from list"""
-   def __init__(self):
-        """This initialises the list"""
+class MyList(list):
+    """This is my list class"""
+
+    def __init__(self):
+        """This initializes the list"""
         super().__init__()
-   def print_sorted(self):
-        """This prints a sorted list"""
-       print(sorted(List))
+
+    def print_sorted(self):
+        """THis prints a sorted list"""
+        print(sorted(self))
