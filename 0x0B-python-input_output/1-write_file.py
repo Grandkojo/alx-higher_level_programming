3b1d02d533e746b3bb708af1cc57ec892c7cc3c5
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 21:12:28 +0000

    Python I/O

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains one function that writes onto a file"""
+
+
+def write_file(filename="", text=""):
+    """This function writes onto a file"""
+    with open(filename, 'w',  encoding="utf-8") as f:
+       written = f.write(text)
+    print(written)
