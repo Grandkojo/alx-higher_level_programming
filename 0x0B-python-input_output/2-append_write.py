b8f550e4faad2665a19673fd6140f3824890327d
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 21:29:42 +0000

    Python I/O

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@
 
 def append_write(filename="", text=""):
     """This function appends unto the file"""
-    if filename is None or type(test) is not str:
+    if filename is None or type(filename) is not str:
         return
     with open(filename, 'a', encoding="utf-8") as f:
         appended = f.write(text)
