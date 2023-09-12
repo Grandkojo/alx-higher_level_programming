a4cec47ff810246184aeb7b9adf2b71fd045107b
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 21:53:35 +0000

    PythonI/O

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This modules has a functin that save an object to json"""
+
+
+def save_to_json_file(my_obj, filename):
+    """This function saves to ajson file"""
+    import json
+    if file is None or type(filename) is not str:
+        return
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(json.JSONEncoder().encode(my_obj))
