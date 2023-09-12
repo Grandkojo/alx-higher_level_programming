77da2bda2be914a526be9789c944272ae781cf88
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 21:33:52 +0000

    Python I/O

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains one function that converts to JSON"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """This function converts the string to JSON string"""
+    print(json.dumps(my_obj))
