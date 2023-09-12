a377fd142b3318c0e5a3b2e15ae0af01e7f593aa
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 20:39:55 +0000

    Python I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This module conitains one function that reads a file""""
+"""This module contains one function that reads a file."""
 
 
 def read_file(filename=""):
-	"""This is a function that reads a file"""
-	with open('filename', encoding="utf-8") as f:
-		print(f.read(), end="")
+    """This is a function that reads a file."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
