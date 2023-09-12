ebb25f2db70678b734be8824eb206ae72273eece
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 21:28:06 +0000

    Python I/O

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains one function that appends unto a file"""
+
+
+def append_write(filename="", text=""):
+    """This function appends unto the file"""
+    if filename is None or type(test) is not str:
+        return
+    with open(filename, 'a', encoding="utf-8") as f:
+        appended = f.write(text)
+    return appended
