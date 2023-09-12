73d569284ff925d1cf2dafd653582a1a66f787fe
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-12 22:13:37 +0000

    Python I/O

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""This module contains a function that creates an object from JSON file"""
+"""This module contains a function that loads a json file"""
 
 
 def load_from_json_file(filename):
-    """This function creates an object from JSON"""
+    """This func loads a json file"""
     import json
     if filename is None or type(filename) is not str:
         return
-    with open(filename, 'r', encoding="utf-8"):
-        read = json.JSONEncoder().decode(f.read())
-    return read
+    a = None
+    with open(filename, "r") as f:
+        a = json.JSONDecoder().decode(f.read())
+    return a
