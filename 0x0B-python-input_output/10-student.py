bbcd86ef28f90d2cdbc1e8b184355a4a64dab5a7
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-13 10:35:47 +0000

    Python I/O

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a class"""
+
+
+class Student():
+    """This is a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """The initializor of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dict repr of a Student"""
+        if attrs is None:
+            attrs = self.__dict__.keys()
+        jsonrepr = {k: v for k, v in self.__dict__.items() if not k.startswith("__")}
+        return jsonrepr
