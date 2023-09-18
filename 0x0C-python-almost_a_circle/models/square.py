57377752aef9055958f5aee79c536f5afb683113
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-18 22:55:57 +0000

    Almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-"""This module is for a square class that inherits from the rectangle class"""
+"""This module is for a Square class that inherits from the Rectangle class"""
 
 
 from .rectangle import Rectangle
 
 
-class Square:
-    """The square class that inherits from the Rectangle class"""
+class Square(Rectangle):
+    """The Square class that inherits from the Rectangle class"""
+    
     def __init__(self, size, x=0, y=0, id=None):
-        """The initializor of the square class"""
+        """The initializer of the Square class"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """string representation of the square"""
-        square_name = "[Square] {()} {()}/{()} - {}".format(self.id, self.x, self.y, self.size)
+        """String representation of the Square"""
+        square_name = "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
         return square_name
 
     @property
     def size(self):
-        """The getter and setter for size"""
+        """Getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter for size"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """The update function of the square class"""
+        """Update function of the Square class"""
         square_attributes = ['id', 'size', 'x', 'y']
         if args:
             for i, arg in enumerate(args):
                 if i == 4:
                     break
-        setattr(self, square_attributes[i], arg)
-
+            setattr(self, square_attributes[i], arg)
         elif kwargs:
             for key, value in kwargs.items():
                 if key in square_attributes:
                     setattr(self, key, value)
 
     def to_dictionary(self):
-        """The dictionary representation of the square"""
-        dict_rep = {'id': self.id, 'size': self.size, 'x':self.x, 'y': self.y}
+        """Dictionary representation of the Square"""
+        dict_rep = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
         return dict_rep
+
