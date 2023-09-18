7e4583d9cdb1cc04e2563808584e601d5582721f
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-09-18 13:43:05 +0000

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,12 @@ class Base:
         else:
            Base. __nb_objects += 1
            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Represent the json representation"""
+        import json
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.JSONEncoder().encode(list_dictionaries)
