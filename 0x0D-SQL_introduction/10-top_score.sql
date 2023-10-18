e01be2c9d96bc5ce485f4fa89ad1c7b5caa86284
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-10-18 13:14:13 +0000

    SQL

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- selects records from `second_table` in order
-SELECT score, name from second_table ORDER BY score DESC;
+-- lists all the records of the second table in order
+SELECT score, name FROM second_table ORDER BY score DESC;
