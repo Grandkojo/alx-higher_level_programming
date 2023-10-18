13ce69bef50ad212d22b357f2a6bc5cfeafb8284
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-10-18 13:11:24 +0000

    SQL

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- selects records from `second_table` in order
-SELECT score, name from second_table ORDER BY score DESC
+SELECT score, name from second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- looks for records with names
-SELECT score, name FROM second_table where name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
