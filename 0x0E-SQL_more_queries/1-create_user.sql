e0853518e441cb1ce48f7a61f33d67b14a60db87
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-10-19 21:23:22 +0000

    More Queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates a user and sets password
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIViLEGES ON *.* TO 'user_0d_1'@'localhost';
