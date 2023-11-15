e409bb12f455abd98402196b33d2ee887fdb3f0c
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-11-15 20:06:41 +0000

    Javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const countargs = process.argv.length;
+if (countargs === 2){
+  console.log('No argument');
+}else if (countargs === 3){
+  console.log('Argument found');
+}
+else {
+  console.log('Arguments found');
+}
