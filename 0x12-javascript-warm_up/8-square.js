2e4b026efee2beaef98c36568089d20486daa565
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-11-15 20:55:22 +0000

    Javascript

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < x, i++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
+}
