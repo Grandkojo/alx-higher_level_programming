3f1b71e5fee960214748b3d8995b77479774c167
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-11-15 21:00:56 +0000

    Javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,6 @@ const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < x, i++) {
+  for (let i = 0; i < x; i++) {
   console.log('C is fun');
-  }
-}
+  }}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < x, i++) {
+  for (let i = 0; i < x; i++) {
     let row = '';
     for (let c = 0; c < size; c++) row += 'X';
     console.log(row);
