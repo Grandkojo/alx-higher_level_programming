f37891c3612a51e5082fcbaccb9cda6dafa2cdfe
Grandkojo <essienernesr.kojoowusu@gmail.com>
2023-12-01 09:39:40 -0800

    Network 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+
+
+import urllib.request
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print(f"/t- type: {type(html)}")
+        print(f"/t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
