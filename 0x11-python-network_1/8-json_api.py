de2579d5694907386ead666dcda3390fa06e4d2f
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-12-03 09:20:32 -0800

    Networking 1

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This scriot is a search API"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+        load = {'q': q}
+        html = requests.post(url, data=load)
+        try:
+            jsonrep = html.json()
+            if jsonrep:
+                print(f"[{jsonrep.get('id')}] {jsonrep.get('name')}")
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a vslid JSON")
