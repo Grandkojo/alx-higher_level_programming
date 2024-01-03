2bb09bccf61596911baa86e7853d1366426d080b
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-01-03 12:24:28 -0800

    Web Scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
+const fileError = `Usage: ${process.argv[1]} <filepath>`;
+const fileContentError = `Usage: ${process.argv[1]} <filepath> <file content>`;
+
+if (!filePath) {
+  console.error(fileError);
+  process.exit(1);
+}
+
+if (!fileContent) {
+  console.error(fileContentError);
+  process.exit(1);
+}
+
+fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('File written succesfully...');
+  }
+});
