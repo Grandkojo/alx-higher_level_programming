b435f5b177eac28268d0ac50737826eb4d38479e
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-01-03 09:43:00 -0800

    Web Scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,17 +2,17 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const file_error = `Usage: ${process.argv[1]} <filename>`;
+const fileError = `Usage: ${process.argv[1]} <filename>`;
 
-if (!filePath){
-	console.error(file_error);
-	process.exit(1);
+if (!filePath) {
+  console.error(fileError);
+  process.exit(1);
 }
 
-fs.readFile(filePath, 'utf-8', (err, data) =>{
-	if (err){
-		console.log(err);
-		return;
-	}
-	return console.log(data);
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  return console.log(data);
 });
