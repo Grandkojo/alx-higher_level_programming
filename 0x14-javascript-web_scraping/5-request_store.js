2dc65b81a4711db2fd236ed7ebbe8239cc895afc
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-01-03 16:19:33 -0800

    Web Scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const reqPage = process.argv[2];
+const filePath = process.argv[3];
+const fileError = `Usage: ${process.argv[1]} <filename>`;
+
+if (!filePath) {
+  console.error(fileError);
+  process.exit(1);
+}
+
+request(reqPage, { json: true }, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  } else {
+    //const store = JSON.stringify(body); // Convert JSON to string
+    const store = body;
+    fs.writeFile(filePath, store, 'utf-8', (error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('File written successfully...');
+      }
+    });
+  }
+});
