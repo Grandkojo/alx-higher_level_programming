9bde035f0601b6ea6e8adc2aba18199a0415bc9a
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-02-04 04:23:38 -0800

    js objects

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bn/node
 exports.esrever = function (list) {
-    const nlist = [];
-    for (let i = list.length - 1; i >= 0; i++) {
-      nlist.push(list[i]);
-    }
-    return nlist;
+  const nlist = [];
+  for (let i = list.length - 1; i >= 0; i++) {
+    nlist.push(list[i]);
+  }
+  return nlist;
 };
